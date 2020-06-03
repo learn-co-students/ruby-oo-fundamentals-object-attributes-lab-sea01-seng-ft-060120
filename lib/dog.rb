3eed4b600fdf384842772fca186171aad6211a32
dbpatnodee <dbpatnode@hotmail.com>
2020-06-03 10:19:52 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+attr_accessor :name, :breed
+
+    def dog_name
+        @name = name
+    end
+
+    def dog_breed
+        @breed = breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+attr_accessor :name, :job
+
+    def person_name
+        @name = name
+    end
+
+    def person_job
+        @job = job
+    end
+end
