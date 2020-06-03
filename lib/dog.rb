9e6fce62e5dcfba69b3ab2243bef9e97faf8caf2
Jlorenc1 <joshua.lorenc@icloud.com>
2020-06-03 09:55:56 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def initialization(name, breed)
+        @name = name
+        @breed = breed
+    end
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+end
